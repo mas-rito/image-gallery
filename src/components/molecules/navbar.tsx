f5ed6428509765a1fe7cd2dbb3296f9f92b4b286
Rito Ramadhan <ritoramadhan8@gmail.com>
2024-05-16 15:16:06 +0700

    style(components): add navbar

---
+++ src/components/molecules/navbar.tsx
@@ -0,0 +1,23 @@
+import { Search } from "lucide-react"
+
+import { Logo } from "../atoms/logo"
+
+export const Navbar = () => {
+  return (
+    <nav className="bg-white py-2 shadow-sm">
+      <div className="container flex items-center justify-between gap-x-5 md:gap-x-0">
+        <Logo />
+        <div className="relative h-full w-full md:w-1/2 lg:w-1/3">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="h-full w-full rounded-full py-2 pe-11 ps-4 text-zinc-700 outline outline-1 outline-zinc-200"
+          />
+          <button className="absolute bottom-0 right-0 top-0 rounded-full bg-red-600 p-2 text-white">
+            <Search />
+          </button>
+        </div>
+      </div>
+    </nav>
+  )
+}
